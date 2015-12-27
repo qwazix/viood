178b1b13b1680d1894b34799ed2e7fc92d4cefb3
Michael Demetriou <qwazix@gmail.com>
2015-12-27 02:15:26 +0200

    added json api for showcase (returns flat json)

--- list.js.php
+++ list.js.php
@@ -3,7 +3,19 @@ require_once 'config.php';
 require_once 'functions.php';
 
 header("Content-type: text/javascript");
+header("Access-Control-Allow-Origin: *");
 
 $galleries = array();
 
-echo "var list=\n".json_encode(recurse_dir($pictureDir, $galleries),JSON_PRETTY_PRINT);
+if (isset($_GET['showcase'])) {
+    $array = flatten(recurse_dir($pictureDir, $galleries, 100 , false, true));
+    shuffle($array);
+    echo json_encode($array);
+} else {
+    echo "var list=\n".json_encode(recurse_dir($pictureDir, $galleries, 100*showcase ,!$showcase, $showcase),JSON_PRETTY_PRINT);
+}
+
+
+
+
+//
